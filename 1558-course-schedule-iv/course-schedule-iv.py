82d430226b5d4859a94312420502ff8535d3b3f4
Hasan Noor <109660327+underscoreHasan@users.noreply.github.com>
2025-01-28 02:07:07 +0500

    Time: 107 ms (43.03%) | Memory: 20.4 MB (47.92%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.py
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -5,29 +5,20 @@ class Solution:
         res = []
         isReq = [[False] * numCourses for _ in range(numCourses)]
         preReqMap = defaultdict(list)
-        queue = []
         
         for p in prerequisites:
             preReqMap[p[0]].append(p[1])
     
-        # for p in preReqMap:
-        #     queue.append(p)
-
-        #     while queue:
-        #         curr = queue.pop(0)
-        #         for next in preReqMap.get(curr, []):
-        #             queue.append(next)
-        #             isReq[curr][next] = True
 
         for prereq, course in prerequisites:
             queue = deque()
             queue.append(prereq)
             while queue:
                 curr = queue.popleft()
-                for neighbor in preReqMap[curr]:
-                    if not isReq[prereq][neighbor]:
-                        isReq[prereq][neighbor] = True
-                        queue.append(neighbor)
+                for next in preReqMap[curr]:
+                    if not isReq[prereq][next]:
+                        isReq[prereq][next] = True
+                        queue.append(next)
 
         for q in queries:
             res.append(isReq[q[0]][q[1]])
