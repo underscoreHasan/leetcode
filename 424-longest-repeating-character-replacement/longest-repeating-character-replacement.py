a563119439e9af7ff3dcbc17f175dc944a14bd31
Hasan Noor <109660327+underscoreHasan@users.noreply.github.com>
2025-02-09 03:36:46 +0500

    Time: 147 ms (43.49%) | Memory: 17.9 MB (88.13%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,26 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        #XYYX k = 2 -> YYYY, XXXX
+        #AAABABB -> AAAAABB, AAABBBB wrong
+        #ABBB k=2 -> BBBB
+        l = r = 0
+        counts = {}
+        res = 0
+
+        while r < len(s):
+            counts[s[r]] = 1 + counts.get(s[r], 0)
+
+            #make sure that current window is valid
+            #it is valid when the window only requires a max of k replacements
+            if ((r-l+1) - max(counts.values())) > k:
+                counts[s[l]] -= 1
+                l += 1
+
+            res = max(res, r-l+1)
+            r+=1
+        
+        return res
+
+
+
+            
