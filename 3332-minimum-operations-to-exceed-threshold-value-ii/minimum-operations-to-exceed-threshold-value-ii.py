51b264511a9fdca02487a1f0d175c83f30c56614
Hasan Noor <109660327+underscoreHasan@users.noreply.github.com>
2025-02-13 11:26:16 +0500

    Time: 259 ms (39.50%) | Memory: 35.2 MB (97.90%) - LeetSync

--- 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.py
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.py
@@ -1,15 +1,13 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        def doOp(x, y):
-            return min(x,y) * 2 + max(x,y)
-
         res = 0
 
         heapq.heapify(nums)
 
         while nums[0] < k:
-            new = doOp(heapq.heappop(nums), heapq.heappop(nums))
-            heapq.heappush(nums, new)
+            x = heapq.heappop(nums)
+            y = heapq.heappop(nums)
+            heapq.heappush(nums, min(x,y) * 2 + max(x,y))
             res += 1
         
         return res
