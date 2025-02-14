d0b9dc12d908a45886e3eeb63e1e4a333810036c
Hasan Noor <109660327+underscoreHasan@users.noreply.github.com>
2025-02-14 07:02:13 +0500

    Time: 23 ms (92.24%) | Memory: 31.6 MB (61.64%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.py
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.py
@@ -1,20 +1,16 @@
 class ProductOfNumbers:
 
     def __init__(self):
-        self.stream = []
         self.prefixMult = [1]
 
     def add(self, num: int) -> None:
-        self.stream.append(num)
-        # if not self.prefixMult:
-        #     self.prefixMult.append(num)
-        if not num:
+        if num == 0:
             self.prefixMult = [1]
         else:
             self.prefixMult.append(self.prefixMult[-1] * num)
 
     def getProduct(self, k: int) -> int:
-        return 0 if (k+1 > len(self.prefixMult)) else int(self.prefixMult[-1] / self.prefixMult[-k-1])
+        return 0 if (k+1 > len(self.prefixMult)) else (self.prefixMult[-1] // self.prefixMult[-k-1])
 
 # Your ProductOfNumbers object will be instantiated and called as such:
 # obj = ProductOfNumbers()
