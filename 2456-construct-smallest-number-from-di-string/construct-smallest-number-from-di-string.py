48841e20cdf09ed8885b58dde74b1dda6d0afb9c
Hasan Noor <109660327+underscoreHasan@users.noreply.github.com>
2025-02-18 11:49:15 +0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (54.04%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
@@ -2,29 +2,12 @@ class Solution:
     def smallestNumber(self, pattern: str) -> str:
         stack = []
         res = []
-        n = len(pattern)
-        i = 0
+        
+        for i in range(len(pattern) + 1):
+            stack.append(str(i + 1))  # Push the next number onto the stack
 
-        while i < n + 1:
-            # Always push the next number onto the stack.
-            stack.append(str(i + 1))
-
-            # If we're at the end of the pattern, flush the stack.
-            if i == n:
+            if i == len(pattern) or pattern[i] == "I":
                 while stack:
-                    res.append(stack.pop())
-            else:
-                # Explicit condition when an "I" is encountered.
-                if pattern[i] == "I":
-                    while stack:
-                        res.append(stack.pop())
-                # Explicit condition when a "D" is encountered.
-                elif pattern[i] == "D":
-                    # When a "D" is encountered, we do not flush the stack.
-                    # The stack will be flushed later when an "I" is encountered
-                    # or at the end of the loop.
-                    pass
-
-            i += 1
-
+                    res.append(stack.pop())  # Pop everything when we see "I" or reach the end
+        
         return "".join(res)
