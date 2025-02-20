ab0ba3429ff08bbcd9cac7e4b7500019a5485c00
Hasan Noor <109660327+underscoreHasan@users.noreply.github.com>
2025-02-20 11:25:53 +0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (20.67%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.py
+++ 2107-find-unique-binary-string/find-unique-binary-string.py
@@ -2,10 +2,13 @@ from collections import Counter
 
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
+        # 0 -> 2
+        # 00 -> 4
+        # iterate over all binary strings, check for their existence in nums
+        # n digits => 2^n strings, O(1) existence check
         n = len(nums)
         numsMap = Counter(nums)
 
-        for i in range(2 ** n):
-            binary_str = bin(i)[2:].zfill(n)  # Convert to binary and pad with leading zeros
-            if binary_str not in numsMap:
-                return binary_str
+        for i in range(2**n):
+            if bin(i)[2:].zfill(n) not in numsMap:
+                return bin(i)[2:].zfill(n)
