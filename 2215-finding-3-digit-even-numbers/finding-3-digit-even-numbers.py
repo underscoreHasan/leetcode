aeaf931436e8673a27ead686799c519ce774632b
Hasan Noor <109660327+underscoreHasan@users.noreply.github.com>
2025-05-12 11:43:43 -0700

    Time: 83 ms (53.72%) | Memory: 18 MB (46.03%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.py
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.py
@@ -1,19 +1,5 @@
 class Solution:
     def findEvenNumbers(self, digits: List[int]) -> List[int]:
-        # import itertools
-
-        #res = []
-        # def checkConditions(n: int):
-        #     if int(str(n)[0]) != 0 and n%2 ==0 and len(str(n)) == 3:
-        #         return True
-        
-        # perms = list(itertools.permutations(digits, 3))
-        # for n in perms:
-        #     if checkConditions(int(''.join(str(x) for x in n))):
-        #         res.append(int(''.join(str(x) for x in n)))
-        
-        # return sorted(res)
-
         res = []
         freq = Counter(map(str, digits))
         for n in range(100, 1000, 2):
