8acfabfbc4da9574863ebe98b6618febc54586ee
Hasan Noor <109660327+underscoreHasan@users.noreply.github.com>
2025-06-05 16:46:10 -0700

    Time: 20 ms (40.20%) | Memory: 18 MB (34.31%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -8,15 +8,15 @@ class Solution:
 
         l, r = 0, 0
 
-        curr = DefaultDict(int)
+        curr = set()
 
         while r < len(s):
-            if curr[s[r]] == 0: # if letter not in dict
-                curr[s[r]] += 1
+            if s[r] not in curr: # if letter not in dict
+                curr.add(s[r])
                 r+=1
                 res = max(res, r-l)
             else: # if already seen
-                curr[s[l]] -= 1
+                curr.remove(s[l])
                 l+=1
         
         return res
