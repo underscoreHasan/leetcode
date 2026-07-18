3ec4997cd6691f803cd41d6d5ed9a32a42104c8f
Hasan Noor <hasan.nr49@gmail.com>
2026-07-18 15:00:06 -0700

    Time: 206 ms (28.50%) | Memory: 34.6 MB (84.71%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -5,15 +5,19 @@ class Solution:
 
         for windowEnd in range(len(nums)):
             windowStart = windowEnd - k + 1
-            
+            # 1. Maintain the decreasing order property
+            # Remove elements from the back that are smaller than the current element
             while q and nums[q[-1]] <= nums[windowEnd]:
                 q.pop()
 
+            # Add the current index to the back
             q.append(windowEnd)
 
+            # 2. repeatedly remove the front element if it's out of window bounds
             while q and q[0] < windowStart:
                 q.popleft()
 
+            # 3. Append the maximum element to our results
             if windowStart >= 0:
                 res.append(nums[q[0]])
 
