2b0e6d9ae1ce3bd56850fb0cc24bd9e28c651249
Hasan Noor <hasan.nr49@gmail.com>
2026-07-18 14:27:09 -0700

    Time: 347 ms (11.53%) | Memory: 46.1 MB (6.95%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,23 +1,17 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         res = []
-
         heap = []
 
-        #init heap
-        for i in range(k):
+        for i in range(0, len(nums)):
             heapq.heappush(heap, (-nums[i], i))
-        if heap: res.append(-heap[0][0])
 
-        #for every startIndex in the sliding window
-        for startIndex in range(1, len(nums) - k + 1):
-            #add the element that just joined
-            heapq.heappush(heap, (-nums[startIndex + k - 1], startIndex + k - 1))
-
+            windowStart = i - k + 1
             #find the max but remove deleted elemts first
-            while heap and heap[0][1] < startIndex:
+            while heap and heap[0][1] < windowStart:
                 val = heapq.heappop(heap)
             
-            if heap: res.append(-heap[0][0])
+            if windowStart >= 0:
+                res.append(-heap[0][0])
 
         return res
