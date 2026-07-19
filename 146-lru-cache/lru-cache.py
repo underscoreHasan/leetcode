5dcbb077bcea2ad98edc0d789005b2e6de38aceb
Hasan Noor <hasan.nr49@gmail.com>
2026-07-19 14:16:42 -0700

    Time: 103 ms (81.99%) | Memory: 76.7 MB (84.59%) - LeetSync

---
+++ 146-lru-cache/lru-cache.py
@@ -0,0 +1,25 @@
+class LRUCache:
+    # last = most recent
+
+    def __init__(self, capacity: int):
+        self.size = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.cache.move_to_end(key, last=True)
+            return self.cache[key]
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        self.cache[key] = value
+        self.cache.move_to_end(key, last=True)
+        if len(self.cache) > self.size: #if cache still has space
+            self.cache.popitem(last=False)
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
